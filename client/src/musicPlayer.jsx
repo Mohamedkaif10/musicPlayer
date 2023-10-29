688c7da42a61dc0feded02b03709ec2d4b7a27dd
MohamedKaif-ft <141742064+MohamedKaif-ft@users.noreply.github.com>
2023-10-29 20:36:29 +0530

    track name and artist included

--- client/src/musicPlayer.jsx
+++ client/src/musicPlayer.jsx
@@ -3,15 +3,19 @@ import React, { useState, useEffect } from 'react';
 const MusicPlayer = () => {
   const [musicTracks, setMusicTracks] = useState([]);
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    
     fetch('http://localhost:8002/music')
       .then((response) => response.json())
       .then((data) => {
         setMusicTracks(data);
+        setLoading(false);
       })
-      .catch((error) => console.error('Error fetching music tracks', error));
+      .catch((error) => {
+        console.error('Error fetching music tracks', error);
+        setLoading(false);
+      });
   }, []);
 
   const playNextTrack = () => {
@@ -26,18 +30,30 @@ const MusicPlayer = () => {
     }
   };
 
+  useEffect(() => {
+    if (musicTracks.length > 0) {
+      console.log(`Now playing: ${musicTracks[currentTrackIndex].title} by ${musicTracks[currentTrackIndex].artist}`);
+    }
+  }, [currentTrackIndex, musicTracks]);
+
   return (
     <div>
       <h2>Music Player</h2>
-      {musicTracks.length > 0 ? (
+      {loading ? (
+        <p>Loading music tracks...</p>
+      ) : musicTracks.length > 0 ? (
         <div className="music-player">
           <audio controls autoPlay>
             <source src={`http://localhost:8002/music/${musicTracks[currentTrackIndex]._id}`} type="audio/mpeg" />
             Your browser does not support the audio element.
           </audio>
           <div className="music-controls">
-            <button onClick={playPreviousTrack} disabled={currentTrackIndex === 0}>Previous</button>
-            <button onClick={playNextTrack} disabled={currentTrackIndex === musicTracks.length - 1}>Next</button>
+            <button onClick={playPreviousTrack} disabled={currentTrackIndex === 0}>
+              Previous
+            </button>
+            <button onClick={playNextTrack} disabled={currentTrackIndex === musicTracks.length - 1}>
+              Next
+            </button>
           </div>
         </div>
       ) : (
@@ -47,7 +63,7 @@ const MusicPlayer = () => {
         <ul className="music-list">
           {musicTracks.map((track, index) => (
             <li key={track._id} className={index === currentTrackIndex ? 'active' : ''}>
-              title by artist
+              {track.title} by {track.artist}
             </li>
           ))}
         </ul>
--- client/src/upload.jsx
+++ client/src/upload.jsx
@@ -4,6 +4,8 @@ const UploadMusicPlayer = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [audioSrc, setAudioSrc] = useState('');
   const [uploadSuccess, setUploadSuccess] = useState(false);
+  const [title, setTitle] = useState('');
+  const [artist, setArtist] = useState('');
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -19,6 +21,8 @@ const UploadMusicPlayer = () => {
     if (selectedFile) {
       const formData = new FormData();
       formData.append('music', selectedFile);
+      formData.append('title', title); 
+      formData.append('artist', artist); 
 
       try {
         const response = await fetch('http://localhost:8002/upload', {
@@ -29,14 +33,11 @@ const UploadMusicPlayer = () => {
         if (response.ok) {
           console.log('Music uploaded successfully');
           setUploadSuccess(true);
-         
         } else {
           console.error('Error uploading music');
-         
         }
       } catch (error) {
         console.error('Error uploading music', error);
-       
       }
     }
   };
@@ -45,6 +46,18 @@ const UploadMusicPlayer = () => {
     <div>
       <h2>Music Upload</h2>
       <input type="file" accept=".mp3" onChange={handleFileChange} />
+      <input
+        type="text"
+        placeholder="Enter title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Enter artist"
+        value={artist}
+        onChange={(e) => setArtist(e.target.value)}
+      />
       <button onClick={handleUpload}>Upload Music</button>
 
       {audioSrc && !uploadSuccess && (
